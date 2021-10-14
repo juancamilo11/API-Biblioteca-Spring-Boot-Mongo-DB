81ee9feb2a3227c57219a14ecf0b4c77352da5a5
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-10-14 13:48:21 -0500

    Creación del método GET Libro por Id

--- src/main/java/dev/j3c/sofkau/biblioteca/controlador/LibroControlador.java
+++ src/main/java/dev/j3c/sofkau/biblioteca/controlador/LibroControlador.java
@@ -1,10 +1,25 @@
 package dev.j3c.sofkau.biblioteca.controlador;
 
+import dev.j3c.sofkau.biblioteca.dto.LibroDTO;
+import dev.j3c.sofkau.biblioteca.servicio.LibroServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/biblioteca")
 public class LibroControlador {
+
+    @Autowired
+    private LibroServicio libroServicio;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LibroDTO> findbyId(@PathVariable("id") String id) {
+        return new ResponseEntity(libroServicio.getLibroPorId(id), HttpStatus.OK);
+    }
 
 }
--- src/main/java/dev/j3c/sofkau/biblioteca/servicio/LibroServicio.java
+++ src/main/java/dev/j3c/sofkau/biblioteca/servicio/LibroServicio.java
@@ -1,8 +1,21 @@
 package dev.j3c.sofkau.biblioteca.servicio;
 
+import dev.j3c.sofkau.biblioteca.mapper.LibroMapper;
+import dev.j3c.sofkau.biblioteca.modelo.Libro;
+import dev.j3c.sofkau.biblioteca.repositorio.LibroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibroServicio {
 
+    @Autowired
+    private LibroRepositorio libroRepositorio;
+
+    private LibroMapper libroMapper = new LibroMapper();
+
+    public Object getLibroPorId(String id) {
+        Libro libro = libroRepositorio.findById(id).orElseThrow(() -> new RuntimeException("Recurso con id " + id + " no encontrado."));
+        return libroMapper.fromCollection(libro);
+    }
 }
