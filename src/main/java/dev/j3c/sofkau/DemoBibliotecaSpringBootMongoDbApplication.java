234094ce1bb4f82f685f60869247f696456b7cf0
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-10-14 03:04:44 -0500

    Inicio del proyecto

---
+++ src/main/java/dev/j3c/sofkau/DemoBibliotecaSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package dev.j3c.sofkau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBibliotecaSpringBootMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBibliotecaSpringBootMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/j3c/sofkau/DemoBibliotecaSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c.sofkau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBibliotecaSpringBootMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
